c0ef6cb311e0c8b99c9d76dedd2bcdae7d0058fd
Tom Okafor <tmeka008@gmail.com>
2025-06-08 22:50:07 +0100

    feat: add path to register page

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,17 @@
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Staff Register</title>
+    <link rel="stylesheet" href='styles.css'>
+</head>
     <header>
         <span>Staff Register</span>
         <nav>
-            <a href="">View All</a>
+            <a href="register.php">View All</a>
             <a href="">Add Member</a>
         </nav>
     </header>
--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digital Staff Register</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
+<?php include_once './includes/header.php';?>
 
 <body>
-<?php include_once './includes/header.php'; ?>
 <main>
         <form action="" method="post">
             <fieldset>
