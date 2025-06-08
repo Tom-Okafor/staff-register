c1de593edd64f935926f9492a512fdb190fc1998
Tom Okafor <tmeka008@gmail.com>
2025-06-08 22:50:51 +0100

    feat: create register page

---
+++ register.php
@@ -0,0 +1,50 @@
+<?php
+
+include_once './includes/header.php';
+/*
+
+- Name
+- Age
+- Sex
+- Preferred Pronouns
+- Role or Position
+- Email Address
+- Phone Number
+- Residential Address
+*/
+?>
+
+<main class="register_main">
+    <h2 class="register_heading">Register Table</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>S/N</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Pronouns</th>
+                <th>Role</th>
+                <th>Phone</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>001</td>
+                <td>Jonathan Offor</td>
+                <td>34</td>
+                <td>jonoffor@outlook.com</td>
+                <td>Male</td>
+                <td>He/Him</td>
+                <td>Managing Director</td>
+                <td>+2339889879977</td>
+                <td>New York, USA</td>
+                
+            </tr>
+        </tbody>
+    </table>
+</main>
+
+<?php include_once './includes/footer.php'; ?>
